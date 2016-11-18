c10fa1ac35ce41adbec9dfc28c18762817c31cbd
ioana.finaru <ioana.finaru@OAK-LAP-0BSH9L>
2016-11-18 09:20:26 +0000

    update build script

--- build/build.ps1
+++ build/build.ps1
@@ -13,20 +13,22 @@ $solutionPath="$buildScriptPath\..\$projectName.sln"
 $csprojPath="$buildScriptPath\..\$projectName"
 $csproj="$csprojPath\$projectName.csproj"
 $msbuild="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+$nunit="$buildScriptPath\..\packages\NUnit.Console.3.0.0\tools\nunit3-console.exe"
 
 
-Write-Host "$solutionPath"
+Write-Host "Build"
 @(Get-SolutionConfigurations $solutionPath) | foreach {
-	Write-Host $_
+	Write-Host " - $_"
     & $msbuild $csproj /p:Configuration=$_ /nologo /verbosity:quiet
 }
 
-exit
+
+Write-Host "Run Tests"
 @(Get-SolutionConfigurations $solutionPath)| foreach {
     Start-Job -Name $configuration -ScriptBlock {
         param($configuration)
  
-        nunit-console.exe /xml:nunit_$configuration.xml /nologo /config:$configuration $csprojPath\bin\$configuration\$projectName.dll
+        $nunit --result:nunit_$configuration.xml --config:$configuration $csprojPath\bin\$configuration\$projectName.dll
     } -ArgumentList $_ 
 }
 Get-Job | Wait-Job
